88fcb13cae061cbb31d1d535059bd4d82ae53052
bsoeun866@gamail.com <soeun823>
2024-07-24 02:07:26 +0900

    ADD : header 드롭다운 추가

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+// 네비 hover 했을때 haeder 늘어나는 거
+function setClassName() {
+	document.getElementsByClassName("header")[0].className += " headerOver";
+}
+function reset(){
+	document.getElementsByClassName("headerOver")[0].classList.remove('headerOver');
+}
