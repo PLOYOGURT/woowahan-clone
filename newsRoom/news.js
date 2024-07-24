87a0a8a3c44de84a03bec2b786c4a95cd1db448a
bsoeun866@gamail.com <soeun823>
2024-07-25 03:22:33 +0900

    DOCS : newsroom 추가

--- newsRoom/news.js
+++ newsRoom/news.js
@@ -1,13 +1,12 @@
 // 네비 hover 했을때 haeder 늘어나는 거
 function setClassName() {
 	document.getElementsByClassName("header")[0].className += " headerOver";
+    // document.getElementByClassName("item")[0].classList.remove('active');
 }
 
 
 function reset(){
 	document.getElementsByClassName("headerOver")[0].classList.remove('headerOver');
+    // document.getElementByClassName("item")[0].className += "active";
 
 }
-
-//main 화면 넘어가는 거
-
